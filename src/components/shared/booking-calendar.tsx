1cc9b33dcca82042dee28b931ee56d869837611e
Dicki Prasetya <dickiprasetya176@gmail.com>
2026-03-16 13:27:50 +0700

    feat: menambahkan kondisi tanggal

--- src/components/shared/booking-calendar.tsx
+++ src/components/shared/booking-calendar.tsx
@@ -6,11 +6,31 @@ type Props = {
 };
 
 export default function BookingCalendar({ date, setDate }: Props) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const holidays = [
+    new Date("2026-01-01"),
+    new Date("2026-03-19"),
+    new Date("2026-04-03"),
+    new Date("2026-05-01"),
+    new Date("2026-05-14"),
+    new Date("2026-05-27"),
+    new Date("2026-06-01"),
+    new Date("2026-08-17"),
+    new Date("2026-12-25"),
+  ];
+
   return (
     <Calendar
       mode="single"
       selected={date}
       onSelect={setDate}
+      disabled={[
+        { before: today }, // tanggal lampau
+        { dayOfWeek: [0, 6] }, // sabtu minggu
+        ...holidays, // hari libur nasional
+      ]}
       className="rounded-md border p-4 w-full"
     />
   );
